9179ee198426cb1ae39b5235c2cc2c697eda618e
alponsgithub <alpons@wxs.nl>
2022-07-27 10:45:29 +0200

    add articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root 'pages#home'
+  root "pages#home"
 
-get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about'
+
+  resources :articles, only: [:show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
