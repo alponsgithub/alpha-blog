8e2b57a4d610d5eff0f4827fe72851d7fa38a534
alponsgithub <alpons@wxs.nl>
2022-07-27 12:41:39 +0200

    Adding articles listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show,:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
